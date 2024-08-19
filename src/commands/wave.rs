2b9273cd642cbb6992be383ad44fb352d7358396
Dragon-Git <1762578117@qq.com>
2024-08-19 23:11:17 +0800

    add daidir search

--- src/commands/wave.rs
+++ src/commands/wave.rs
@@ -1,5 +1,5 @@
 use clap::Args;
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 #[derive(Args, Debug)]
 pub struct WaveCommand {
@@ -14,21 +14,41 @@ pub struct WaveCommand {
 impl WaveCommand {
     pub fn run(&self) {
         if let Some(filelist) = &self.filelist {
-        std::process::Command::new("vericom")
-            .args(["-sv", "-ntb_opts", "uvm", "-f", filelist])
-            .current_dir(self.path.clone())
-            .spawn()
-            .expect("vericom command failed to start");
-        std::process::Command::new("elabcom")
-            .args(["-lib", "work.lib++", "-elab", "kdb"])
-            .current_dir(self.path.clone())
-            .spawn()
-            .expect("elabcom command failed to start");
+            let filelist_str = match filelist.to_str() {
+                Some(s) => s,
+                None => panic!("Failed to convert PathBuf to &str"),
+            };
+            std::process::Command::new("vericom")
+                .args(["-sv", "-ntb_opts", "uvm", "-f", filelist_str])
+                .current_dir(self.path.clone())
+                .spawn()
+                .expect("vericom command failed to start\n");
+            std::process::Command::new("elabcom")
+                .args(["-lib", "work.lib++", "-elab", "kdb"])
+                .current_dir(self.path.clone())
+                .spawn()
+                .expect("elabcom command failed to start\n");
+        }
+        // 搜索子目录，直到找到以 .daidir 结尾的目录并且该目录包含 kdb.elab++ 目录
+        let mut daidir = "simv.daidir".to_string();
+        if let Ok(entries) = fs::read_dir(self.path.clone()) {
+            for entry in entries {
+                if let Ok(entry) = entry {
+                    let entry_path = entry.path();
+                    if entry_path.is_dir() && entry_path.extension().unwrap() == "daidir" {
+                        let kdb_path = entry_path.join("kdb.elab++");
+                        if kdb_path.exists() && kdb_path.is_dir() {
+                            daidir = entry_path.to_str().unwrap().to_string();
+                            break;
+                        }
+                    }
+                }
+            }
         }
         std::process::Command::new("verdi")
-            .arg(["-q", "-nologo", "-dbdir", "simv.daidir", "-ssf", "top.fsdb"])
+            .args(["-q", "-nologo", "-dbdir", daidir.as_ref(), "-ssf", "top.fsdb"])
             .current_dir(self.path.clone())
             .spawn()
-            .expect("verdi command failed to start");
+            .expect("verdi command failed to start\n");
     }
 }
