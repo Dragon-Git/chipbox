781c0959219b9f8dd31ad6a1bccfeabf3851c87b
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-08-20 09:26:25 +0800

    更新 mod.rs

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,6 +1,8 @@
 pub mod completion;
+pub mod lint;
 pub mod sim;
 pub mod wave;
 pub use completion::CompletionCommand;
+pub use lint::LintCommand;
 pub use sim::SimCommand;
 pub use wave::WaveCommand;
