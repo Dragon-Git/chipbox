c4514795c941e81ed1f423592f4512e5d9afe3f0
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-08-20 09:24:45 +0800

    更新 commands

---
+++ src/commands/lint.rs
@@ -0,0 +1,26 @@
+use clap::Args;
+use std::path::PathBuf;
+
+#[derive(Args, Debug)]
+pub struct LintCommand {
+    /// set a tool
+    #[arg(short, long)]
+    pub tool: Option<String>,
+
+    /// script path
+    pub path: PathBuf,
+}
+
+impl LintCommand {
+    pub fn run(&self) {
+        std::process::Command::new("jg")
+            .args([
+                "-lint",
+                "-nogui",
+                self.path.clone(),
+            ])
+            .current_dir(".")
+            .spawn()
+            .expect("jaspergold command failed to start\n");
+    }
+}
