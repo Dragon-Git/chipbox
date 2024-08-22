4ce2c2874faa241b87732a20e1d0812f483bc61c
Dragon-Git <42856316+Dragon-Git@users.noreply.github.com>
2024-08-23 07:14:06 +0800

    更新 lint.rs

--- src/commands/lint.rs
+++ src/commands/lint.rs
@@ -14,7 +14,7 @@ pub struct LintCommand {
 impl LintCommand {
     pub fn run(&self) {
         std::process::Command::new("jg")
-            .args(["-lint", "-nogui", self.path.to_str().expect("Invalid path")])
+            .args(["-superlint", "-nogui", self.path.to_str().expect("Invalid path")])
             .current_dir(".")
             .spawn()
             .expect("jaspergold command failed to start\n");
